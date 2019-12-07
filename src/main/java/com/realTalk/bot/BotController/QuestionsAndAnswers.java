284c42d0e0db8077d9e3d24d144182418986fa04
jayant <jayant.kumar@ftechiz.com>
2019-12-07 15:38:29 +0530

    Controller for adding Questions and Answers for survey process

---
+++ src/main/java/com/realTalk/bot/BotController/QuestionsAndAnswers.java
@@ -0,0 +1,25 @@
+package com.realTalk.bot.BotController;
+
+import com.realTalk.bot.Service.QuestionsAndAnswersService;
+import com.realTalk.bot.helpers.Request.QuestionRequest;
+import com.realTalk.bot.helpers.Response.CustomResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/Questions")
+public class QuestionsAndAnswers {
+
+    @Autowired
+    QuestionsAndAnswersService service;
+
+    @GetMapping
+    public ResponseEntity<CustomResponse> createQuestions(@RequestBody QuestionRequest request){
+
+          return service.addQuestionAndAnswers(request);
+
+    }
+
+}
