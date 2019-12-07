da6b18c7d79d1d459a3b323d54ba3bd829790dfd
jayant <jayant.kumar@ftechiz.com>
2019-12-07 15:39:35 +0530

    Functionality for sending Authentication questions added,Also functionality for welcome message added

--- src/main/java/com/realTalk/bot/BotController/SlackBot.java
+++ src/main/java/com/realTalk/bot/BotController/SlackBot.java
@@ -2,6 +2,8 @@ package com.realTalk.bot.BotController;
 
 
 
+import com.realTalk.bot.Model.Questions;
+import com.realTalk.bot.Service.QuestionsAndAnswersService;
 import me.ramswaroop.jbot.core.slack.Bot;
 import me.ramswaroop.jbot.core.slack.Controller;
 import me.ramswaroop.jbot.core.slack.EventType;
@@ -9,12 +11,13 @@ import me.ramswaroop.jbot.core.slack.models.Event;
 import me.ramswaroop.jbot.core.slack.models.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketSession;
 
-
-
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Component
@@ -25,7 +28,8 @@ public class SlackBot extends Bot {
     @Value("${slackBotToken}")
     String slackToken;
 
-
+    @Autowired
+    QuestionsAndAnswersService QueryService;
 
     @Override
     public String getSlackToken() {
@@ -37,12 +41,52 @@ public class SlackBot extends Bot {
         return this;
     }
 
-
-    @Controller(events = {EventType.DIRECT_MENTION, EventType.DIRECT_MESSAGE})
+    List<Questions> questions = new ArrayList<>();
+    int count = 0;
+    @Controller(events = {EventType.DIRECT_MENTION,EventType.DIRECT_MESSAGE})
     public void onReceiveDM(WebSocketSession session, Event event) {
-        if (event.getText().contains("hi")) {
-            reply(session, event, new Message("Hi, I am MOHIT " + slackService.getCurrentUser().getName()));
+
+        if (event.getText().contains("yes") && count == 0) {
+            questions = QueryService.getAuthQuestions("Authentication");
+            count++;
+            reply(session, event, new Message(questions.get(count).getQuestion()));
+            return;
         }
+        if (event.getText().contains("no") && count == 0) {
+            questions = QueryService.getAuthQuestions("Authentication");
+            count++;
+            reply(session, event, new Message("Your Account is not confirmed!\nHave a nice day"));
+            return;
+        }
+
+        if(count == 1){
+            logger.info("reply By user : "+event.getText().toString());
+            count++;
+            reply(session, event, new Message(questions.get(count).getQuestion()));
+            return;
+        }
+        if(count == 2){
+            logger.info("reply By user : "+event.getText().toString());
+            count++;
+            reply(session, event, new Message(questions.get(count).getQuestion()));
+            return;
+        }  if(count == 3){
+            logger.info("reply By user : "+event.getText().toString());
+            count++;
+            reply(session, event, new Message(questions.get(count).getQuestion()));
+            return;
+        }  if(count == 4){
+            logger.info("reply By user : "+event.getText().toString());
+            count++;
+            reply(session, event, new Message(questions.get(count).getQuestion()));
+            return;
+        }  if(count == 5){
+            logger.info("reply By user : "+event.getText().toString());
+            count++;
+            reply(session, event, new Message("Thankyou for your precious Time.\nWe will get back back to you with Account confirmation Information.\nHave a nice Day!"));
+            return;
+        }
+
     }
 
 
@@ -51,4 +95,17 @@ public class SlackBot extends Bot {
         reply(session, event, new Message("Hi, this is a message!"));
     }
 
+
+    @Controller(events = EventType.BOT_ADDED)
+    public void WelcomeMessage(WebSocketSession session, Event event) {
+        reply(session, event, new Message("Hi,Welcome to RealTalk for proving your Authentication" +
+                "you have to answer some really simple Questions.Are you ready?"));
+    }
+
+    @Controller(events = EventType.PIN_ADDED)
+    public void WelcomeMessage2(WebSocketSession session, Event event) {
+        reply(session, event, new Message("Hi,Welcome to RealTalk for proving your Authentication" +
+                " you have to answer some really simple Questions.Are you ready?" +
+                "\n Type yes to proceed else no."));
+    }
 }
