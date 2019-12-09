91625595d078e962d30586ea22c10ee004754170
jayant <jayant.kumar@ftechiz.com>
2019-12-09 18:42:52 +0530

    Date validator used for checking format of date

---
+++ src/main/java/com/realTalk/bot/Utilities/DateValidatorUsingDateFormat.java
@@ -0,0 +1,25 @@
+package com.realTalk.bot.Utilities;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class DateValidatorUsingDateFormat  {
+    private String dateFormat;
+
+    public DateValidatorUsingDateFormat(String dateFormat) {
+        this.dateFormat = dateFormat;
+    }
+
+
+    public boolean isValid(String dateStr) {
+        DateFormat sdf = new SimpleDateFormat(this.dateFormat);
+        sdf.setLenient(false);
+        try {
+            sdf.parse(dateStr);
+        } catch (ParseException e) {
+            return false;
+        }
+        return true;
+    }
+}
