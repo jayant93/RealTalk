d853b67bd58a796bb3066afcb57d7014fb9c8338
jayant <jayant.kumar@ftechiz.com>
2019-12-09 18:42:15 +0530

    changes for timestamp and answer given by

--- src/main/java/com/realTalk/bot/Model/Answers.java
+++ src/main/java/com/realTalk/bot/Model/Answers.java
@@ -1,9 +1,12 @@
 package com.realTalk.bot.Model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
 
 @Entity
 public class Answers {
@@ -15,12 +18,24 @@ public class Answers {
 
     private String Answers;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy hh:mm:ss")
+    private LocalDateTime answerGivenAt = LocalDateTime.now();
+
+
+    private String givenBy;
+
     @ManyToOne
     private Questions question;
 
-    public long getId() {
-        return id;
+    public String getGivenBy() {
+        return givenBy;
     }
+
+    public void setGivenBy(String givenBy) {
+        this.givenBy = givenBy;
+    }
+
+
 
     public String getAnswers() {
         return Answers;
